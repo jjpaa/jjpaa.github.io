be4b7ab400739e09b77f2d7bf985f8c8bc68659c
JJPaa <juhojarvenpaa98@gmail.com>
2024-12-10 20:48:13 +0200

    temp

--- scrollText.js
+++ scrollText.js
@@ -1,12 +1,40 @@
-let contentString = createContent()
-let chars = contentString.split("")
-document.getElementById('scroll-text').innerHTML = chars.join('<br>');
+binaryFall()
+
+function binaryFall()
+{
+
+  let divIdHeader = "dynamicDiv"
+  let divName = ""
+
+  for (let i = 0; i < 10; i++) {
+    divName = divIdHeader.concat(i)
+    var elemDiv = document.createElement('div');
+    elemDiv.className = "scroll-text"
+    elemDiv.innerHTML = createText()
+
+    let randRight = getRandomInt(100) + 1
+    let randBottom = getRandomInt(100) + 1
+
+    elemDiv.style.cssText = 'position:absolute;right:' + randRight +'%;bottom:' + randBottom + '%;opacity:1.0;z-index:100;background:#000;'
+
+    document.getElementById('scroll-container').appendChild(elemDiv);
+  } 
+}
+
+function createText()
+{
+  let contentString = createContent()
+  let chars = contentString.split("")
+  let returnString = chars.join('<br>');
+  return returnString
+}
 
 function createContent()
 {
     let myString = ""
+    let length = getRandomInt(10)
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < length; i++) {
         let randomValue = getRandomInt(2)
         myString = myString.concat(randomValue)
       } 
@@ -15,5 +43,5 @@ function createContent()
 }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+  return Math.floor(Math.random() * max);
+}
